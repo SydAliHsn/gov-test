5ece2ff080a06ab48d78b177a2782234370af88f
Adnan Khan <lodhi79@gmail.com>
2023-08-14 11:57:51 +1000

    Initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Govsites</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://govsites.com.au",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Cumulative Consulting Pty Ltd - 2023. All rights reserved.",
   },
-}
+};
 
-export default config
+export default config;
