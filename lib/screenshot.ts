5c142b9de542a15927f07855a73d429cb415c243
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-17 19:16:54 +0500

    Testing puppeteer with sandbox options

--- lib/screenshot.ts
+++ lib/screenshot.ts
@@ -2,7 +2,9 @@ import puppeteer from 'puppeteer';
 import path from 'path';
 
 const screenshot = async (url: string) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
