9b07eb0d11e607a33de53ce2fc3f6a147273d93b
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-17 19:56:40 +0500

    Screenshot final test

--- lib/screenshot.ts
+++ lib/screenshot.ts
@@ -1,6 +1,8 @@
 import puppeteer from 'puppeteer';
 import path from 'path';
 
+import { generateFileName } from './utils';
+
 const screenshot = async (url: string) => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -9,11 +11,17 @@ const screenshot = async (url: string) => {
 
   await page.goto(url);
 
-  await page.screenshot({ path: path.join(process.cwd(), `/public/screenshots/ali.jpg`) });
+  const fileName = generateFileName(url) + '.jpg';
+
+  await page.screenshot({
+    path: path.join(process.cwd(), `/public/screenshots/${fileName}`),
+    type: 'jpeg',
+    quality: 50,
+  });
 
   await browser.close();
 
-  return '/screenshots/ali.jpg';
+  return `/screenshots/${fileName}`;
 };
 
 export default screenshot;
