3389bd8b947ce434fd93f560eaf28fbabff247ee
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-18 15:58:27 +0500

    Add shadow on ScreenshotImage

--- components/ScreenshotImage.tsx
+++ components/ScreenshotImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 
 const ScreenshotImage = ({ imageUrl }: { imageUrl: string }): JSX.Element => {
-    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg"><Image src={imageUrl} fill={true} alt="ScreenshotImage" /></div>
+    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg shadow-md shadow-gray-400 dark:shadow-none"><Image src={imageUrl} fill={true} alt="ScreenshotImage" /></div>
 };
 
 export default ScreenshotImage;
