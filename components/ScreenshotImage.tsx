76e05cd5d63261a33737cf4fa602e94423d2d742
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-18 15:35:35 +0500

    Add alt text

--- components/ScreenshotImage.tsx
+++ components/ScreenshotImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 
 const ScreenshotImage = ({ imageUrl }: { imageUrl: string }): JSX.Element => {
-    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg"><Image src={imageUrl} fill={true} alt="" /></div>
+    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg"><Image src={imageUrl} fill={true} alt="ScreenshotImage" /></div>
 };
 
 export default ScreenshotImage;
