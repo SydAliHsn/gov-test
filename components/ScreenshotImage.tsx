d0a301dace6baa712e6e402a3667f8e0d5a4b138
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-18 16:27:49 +0500

    Add warning about programmatic screenshots

--- components/ScreenshotImage.tsx
+++ components/ScreenshotImage.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image';
 
 
 const ScreenshotImage = ({ imageUrl }: { imageUrl: string }): JSX.Element => {
-    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg shadow-md shadow-gray-400 dark:shadow-none"><Image src={imageUrl} fill={true} alt="ScreenshotImage" /></div>
+    return <> <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg shadow-md shadow-gray-400 dark:shadow-none"><Image src={imageUrl} fill={true} alt="ScreenshotImage" />
+    </div>
+        <p className='pt-4'>Note: These screenshots are programatically generated. The actual website might look a little different when you browse it from your device.</p>
+    </>
 };
 
 export default ScreenshotImage;
