8f0127b6ab6adaa4d5be43692a3109f2116a2dd0
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-18 15:33:06 +0500

    Create ScreenshotImage component

---
+++ components/ScreenshotImage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+
+
+const ScreenshotImage = ({ imageUrl }: { imageUrl: string }): JSX.Element => {
+    return <div className="w-full aspect-video relative mt-8 overflow-hidden rounded-lg"><Image src={imageUrl} fill={true} alt="" /></div>
+};
+
+export default ScreenshotImage;
