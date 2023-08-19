474bcabced728051ebf8e24a9158087b2ba7d4ca
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-19 15:18:59 +0500

    Change sitemap siteUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://govsites.com.au',
+  siteUrl: process.env.SITE_URL || 'https://www.govsites.com.au',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
