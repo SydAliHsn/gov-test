3d6a7189e497c9f21d5c435a926da3a4878bf4ea
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-19 15:55:24 +0500

    Change links

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,15 +4,18 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span style={{ fontSize: '1.75rem', fontWeight: '600' }}> Govsites</ span>,
   project: {
-    link: "https://govsites.com.au",
+    link: "https://github.com/capex/govsites",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/capex/govsites/tree/main/",
   footer: {
     text: "© Cumulative Consulting Pty Ltd - 2023. All rights reserved.",
   },
+  editLink: {
+    text: "Edit this page on Github",
+  }
 };
 
 export default config;
