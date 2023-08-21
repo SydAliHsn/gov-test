c1780f98517b08daa5fb8e35550d289d226045de
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-21 18:51:24 +0500

    Fix: not get detected as bot and wait for loading screen in puppeteer screenshot.

--- lib/screenshot.ts
+++ lib/screenshot.ts
@@ -21,11 +21,19 @@ const screenshot = async (url: string) => {
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
-    // const screenshotPromises = urls.map(async url => {
     const page = await browser.newPage();
+
+    // To prevent bot detection
+    page.setUserAgent(
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+    );
+
     await page.goto(url, { timeout: 60 * 1000 });
 
     page.setViewport({ width: 1366, height: 768 });
+
+    // Wait for 1.75 seconds to load the page
+    await page.waitForTimeout(1000 * 1.75);
 
     await page.screenshot({
       path: filePath,
